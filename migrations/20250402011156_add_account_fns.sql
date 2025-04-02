fe151ef964c3bb6dbb2d50eda9a7a33a3652b355
Juan Olvera <juan@jolvera.dev>
2025-04-01 20:15:12 -0500

    chore: move add_account function to new migration file

--- migrations/20250402011156_add_account_fns.sql
+++ migrations/20250402011156_add_account_fns.sql
@@ -1,9 +1,35 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+-- function to create a new account
+create or replace function api.add_account(
+    p_ledger_id int,
+    p_name text,
+    p_type text
+) returns int as
+$$
+declare
+    v_account_id int;
+    v_internal_type text;
+begin
+    -- determine internal type based on account type
+    if p_type = 'asset' then
+        v_internal_type := 'asset_like';
+    else
+        v_internal_type := 'liability_like';
+    end if;
+    
+    -- create the account
+    insert into data.accounts (ledger_id, name, type, internal_type)
+    values (p_ledger_id, p_name, p_type, v_internal_type)
+    returning id into v_account_id;
+    
+    return v_account_id;
+end;
+$$ language plpgsql;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+-- drop the function
+drop function if exists api.add_account(int, text, text);
 -- +goose StatementEnd
