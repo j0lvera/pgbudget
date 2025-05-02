d396cb74a104cf0eae8af7a615818879f6dbbe03
Juan Olvera <juan@jolvera.dev>
2025-05-02 18:51:15 -0500

    refactor: add rls

--- migrations/20250326192940_add_ledgers_table.sql
+++ migrations/20250326192940_add_ledgers_table.sql
@@ -25,10 +25,30 @@ create trigger ledgers_updated_at_tg
     on data.ledgers
     for each row
 execute procedure utils.set_updated_at_fn();
+
+-- allow authenticated user to access the ledgers table.
+grant all on data.ledgers to pgb_web_user;
+grant usage, select on sequence data.ledgers_id_seq to pgb_web_user;
+
+-- enable RLS
+alter table data.ledgers
+    enable row level security;
+
+create policy ledgers_policy on data.ledgers using
+    (
+        exists(select 1
+                 from auth.users u
+                where u.id = utils.get_user()
+                  and u.id = data.ledgers.user_id)
+        ) with check (user_id = utils.get_user());
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+drop policy if exists ledgers_policy on data.ledgers;
+
+revoke all on data.ledgers from pgb_web_user;
+
 drop trigger if exists ledgers_updated_at_tg on data.ledgers;
 
 drop table data.ledgers;
