69305316a746618645b9a0a33c321310e675e840
Juan Olvera <juan@jolvera.dev>
2025-05-03 12:27:36 -0500

    feat: add ledgers api functions for creating ledgers

--- migrations/20250503172238_add_ledgers_api_fns.sql
+++ migrations/20250503172238_add_ledgers_api_fns.sql
@@ -1,9 +1,62 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+
+-- function to create a new ledger
+create or replace function utils.ledgers_insert_single(
+    p_user_data text,
+    p_name text,
+    p_description text default null
+)
+    returns table
+            (
+                uuid        text,
+                name        text,
+                description text,
+                metadata    jsonb
+            )
+as
+$$
+begin
+    -- insert and return the requested fields in one operation
+    return query
+        insert into data.ledgers (user_data, name, description)
+            values (p_user_data, p_name, p_description)
+            returning ledgers.uuid, ledgers.name, ledgers.description, ledgers.metadata;
+end;
+$$ language plpgsql;
+
+-- api function to create a new ledger
+create or replace function api.add_ledger(
+    p_name text,
+    p_description text default null
+)
+    returns table
+            (
+                uuid        text,
+                name        text,
+                description text,
+                metadata    jsonb
+            )
+as
+$$
+begin
+    return query
+        select *
+          from utils.ledgers_insert_single(
+                  utils.get_user(),
+                  p_name,
+                  p_description
+               );
+end;
+$$ language plpgsql;
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+
+-- drop the functions
+drop function if exists api.add_ledger(text, text);
+drop function if exists utils.ledgers_insert_single(text, text, text);
+
 -- +goose StatementEnd
