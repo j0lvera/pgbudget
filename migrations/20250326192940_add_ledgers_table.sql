41dae752241632804211184d5786a71cfd80dbd5
Juan Olvera <juan@jolvera.dev>
2025-05-03 14:56:36 -0500

    refactor: update ledgers table unique constraint to prevent duplicate ledger names per user

--- migrations/20250326192940_add_ledgers_table.sql
+++ migrations/20250326192940_add_ledgers_table.sql
@@ -16,7 +16,7 @@ create table data.ledgers
     user_data   text        not null default utils.get_user(),
 
     constraint ledgers_uuid_unique unique (uuid),
-    constraint ledgers_uuid_name_unique unique (uuid, name),
+    constraint ledgers_name_user_unique unique (name, user_data),
     constraint ledgers_name_length_check check (char_length(name) <= 255),
     constraint ledgers_user_data_length_check check (char_length(user_data) <= 255),
     constraint ledgers_description_length_check check (char_length(description) <= 255)
