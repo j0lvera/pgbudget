006710618bc9f1ee584d6937feb73e4f2adfbeeb
Juan Olvera <juan@jolvera.dev>
2025-05-10 20:54:13 -0500

    fix: resolve ambiguous column reference in api.get_budget_status join condition

--- migrations/20250506231415_add_balances_views.sql
+++ migrations/20250506231415_add_balances_views.sql
@@ -52,7 +52,7 @@ begin
         bs.activity::bigint,
         bs.balance::bigint
     from utils.get_budget_status(v_ledger_id) bs
-    join data.accounts a on bs.id = a.id
+    join data.accounts a on a.id = bs.id
     where a.user_data = v_user_data;
 end;
 $$ language plpgsql stable security invoker;
